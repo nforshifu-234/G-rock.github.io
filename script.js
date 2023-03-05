cbc08222b7cda7a5a2ec6e952fcc91f853e083bb
Gglovie <joantetra@gmail.com>
2023-03-05 20:13:22 +0100

    Third commit

---
+++ script.js
@@ -0,0 +1,13 @@
+// var one = document.getElementById('one');
+// var two = document.getElementById('two');
+// var three = document.getElementById('three');
+// var four = document.getElementById('four');
+// var five = document.getElementById('five');
+// var submit = document.getElementById('submit');
+
+// one.addEventListener('click', one_func()) 
+
+// function one_func(){
+//     alert(1);
+
+// }
